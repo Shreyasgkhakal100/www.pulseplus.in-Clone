c242c95e288f5cac14965aab84b86e95963ca415
Khushboo K <30701525+dipthic@users.noreply.github.com>
2021-12-16 18:54:41 +0530

    signup.js

---
+++ signup.js
@@ -0,0 +1,26 @@
+document.querySelector("button").addEventListener("click",signUp);
+var userData = JSON.parse(localStorage.getItem("database")) || [];
+function signUp(event){
+    event.preventDefault();
+    var name = document.querySelector("#user").value;
+    var mobile = document.querySelector("#mobile").value;
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#password").value;
+
+    var userDetails = {
+        userName: name,
+        userMobile: mobile,
+        userEmail: email,
+        userPassword: password,
+    };
+    userData.push(userDetails);
+    console.log(userData);
+    localStorage.setItem("database",JSON.stringify(userData));
+    alert("You Have Successfully Created Your Account.")
+    window.location.href = "login.html"
+
+}
+
+function openCode(){
+    document.querySelector("#ref").style.visibility = "visible";
+}
