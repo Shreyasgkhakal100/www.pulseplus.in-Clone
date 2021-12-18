32d99dd500531c58820899433dfcdda80ba40f2b
Khushboo K <30701525+dipthic@users.noreply.github.com>
2021-12-18 15:41:45 +0530

    update signup.js

--- signup.js
+++ signup.js
@@ -1,17 +1,20 @@
 document.querySelector("button").addEventListener("click",signUp);
 var userData = JSON.parse(localStorage.getItem("database")) || [];
+
 function signUp(event){
     event.preventDefault();
     var name = document.querySelector("#user").value;
     var mobile = document.querySelector("#mobile").value;
     var email = document.querySelector("#email").value;
     var password = document.querySelector("#password").value;
+    
 
     var userDetails = {
         userName: name,
         userMobile: mobile,
         userEmail: email,
         userPassword: password,
+        userCustomerId: Math.floor(100000 + Math.random() * 900000)
     };
     userData.push(userDetails);
     console.log(userData);
